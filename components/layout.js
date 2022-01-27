e66f0eb0ba72fdd323c6b35b8e0771a6ee329e4f
sakshisarang <sakshisarang420@gmail.com>
2022-01-27 04:59:18 -0800

    css changes are made

--- components/layout.js
+++ components/layout.js
@@ -2,61 +2,68 @@ import Link from 'next/link'
 import { useState } from "react";
 import Footer from './Footer';
 import styles from "../styles/layout.module.css";
+// import styles2 from '../styles/Home.module.css';
 export default function Layout({ children }) {
 
     const [isOpen, setIsOpen] = useState(false);
     const openMenu = () => setIsOpen(!isOpen);
     return <>
+    
+
         <nav className={styles.navbar}>
-
-        {/* logo here */}
             <div className={styles.logo}>
-                <a href='/' className={styles.logo}>Logo
-                </a>
+                <Link href='/'>
+                    <a className={styles.logo}>Logo</a>
+                </Link>
             </div>
 
 
-        {/* navbar elements */}
-            <div className={styles.navBtn}>
-                <li className={isOpen === false ?
+
+            {/* navbar elements */}
+            <div className={styles.navBtn} className={isOpen === false ?
                     styles.navmenu : styles.navmenu + ' ' + styles.active}>
-                        <a href='/' className={styles.navitem} className={isOpen === false ?
-                                styles.navlink : styles.navlink + ' ' + styles.active}
-                                onClick={openMenu}>Home
-                        </a>
                     
-                        <a href='/topnews' className={styles.navitem} className={isOpen === false ?
+                        <Link href='/' className={styles.navitem}>
+                            <a className={isOpen === false ?
                                 styles.navlink : styles.navlink + ' ' + styles.active}
-                                onClick={openMenu}>Top News
-                        </a>
-            
-                
-                        <a href='/magazine' className={styles.navitem} className={isOpen === false ?
+                                onClick={openMenu}>Home</a>
+                        </Link>
+                        <Link href='/topnews' className={styles.navitem}>
+                            <a className={isOpen === false ?
                                 styles.navlink : styles.navlink + ' ' + styles.active}
-                                onClick={openMenu}>Magazine
-                        </a>
+                                onClick={openMenu}>Top News</a>
+                        </Link>
                     
-                        <a href='/magazine' className={styles.navitem} className={isOpen === false ?
-                                styles.navlink : styles.navlink + ' ' + styles.active}
-                                onClick={openMenu}>Podcast
-                        </a>
                     
-                        <a href='/magazine' className={styles.navitem} className={isOpen === false ?
+                        <Link href='/magazine' className={styles.navitem}>
+                            <a className={isOpen === false ?
                                 styles.navlink : styles.navlink + ' ' + styles.active}
-                                onClick={openMenu}>About Us
-                        </a>
-                </li>
-            </div>
+                                onClick={openMenu}>Magazine</a>
+                        </Link>
+                    
+                    
+                        <Link href='/magazine' className={styles.navitem}>
+                            <a className={isOpen === false ?
+                                styles.navlink : styles.navlink + ' ' + styles.active}
+                                onClick={openMenu}>Podcast</a>
+                        </Link>
+                        <Link href='/magazine' className={styles.navitem}>
+                            <a className={isOpen === false ?
+                                styles.navlink : styles.navlink + ' ' + styles.active}
+                                onClick={openMenu}>About Us</a>
+                        </Link>
+                    </div>
 
 
-        {/* search box */}
+
+            {/* search box */}
             <div className={styles.search}>
                 <h2>Search</h2>
             </div>
 
 
-        {/* responsive bar  */}
-        <div>
+
+            {/* responsive navbar */}
             <button className={isOpen === false ?
                 styles.hamburger : styles.hamburger + ' ' + styles.active}
                 onClick={openMenu}
@@ -67,9 +74,8 @@ export default function Layout({ children }) {
                 <span className={styles.bar}></span>
                 <span className={styles.bar}></span>
             </button>
-        </div>
         </nav>
-
+        {/* </header> */}
         {children}
         <Footer />
     </>
