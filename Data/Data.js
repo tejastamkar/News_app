1d6cdb6c82b253d63c63276f7a279377e736a475
manishtalele <manishtalele143@gmail.com>
2022-03-08 02:04:41 +0530

    added data tables
    Co-authored-by: tejastamkar <tejastamkar@users.noreply.github.com>

---
+++ Data/Data.js
@@ -0,0 +1,7 @@
+export let Data = [
+  { id: 1, name: "manish", class: 2, title: "rte" },
+  { id: 2, name: "manish", class: 5, title: "def" },
+  { id: 3, name: "manish", class: 9, title: "abc" },
+  { id: 4, name: "manish", class: 6, title: "abc" },
+  { id: 5, name: "manish", class: 3, title: "abc" },
+];
