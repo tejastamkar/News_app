3674ef89f80ae2511bfcc957f7dc9e5f1a47e1f2
Tejas Tamkar <tejas.tamkar@gmail.com>
2022-03-07 15:31:51 +0530

    added DataTablepage

---
+++ pages/Datatable.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Footer from "../Components/Footer";
+import Navbar from "../Components/navBar";
+
+export default function Datatable() {
+  return (
+    <div>
+      <Navbar />
+
+      <Footer />
+    </div>
+  );
+}
