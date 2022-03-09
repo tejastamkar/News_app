df4c58031b6359523df0f6aa297a830b37d30ca9
manishtalele <manishtalele143@gmail.com>
2022-03-09 18:45:53 +0530

    added css for table

--- Components/tables.js
+++ Components/tables.js
@@ -1,49 +1,79 @@
-import { Data } from "../Data/Data";
+// import { Data } from "../Data/Data";
+import styles from "../styles/tables.module.scss";
+import { useState, useEffect } from "react";
+import { db } from "../Firebase";
+import { collection, getDocs } from "firebase/firestore";
+
+async function getDataBase({ Data, setMyLoader, DBName }) {
+  await getDocs(collection(db, DBName)).then(async (snapshort) => {
+    snapshort.docs.forEach((doc) => {
+      Data.push({ ...doc.data(), id: doc.id });
+      setMyLoader(false);
+    });
+  });
+
+  return { Data, setMyLoader };
+}
 
 export default function Tables({ TableIndex }) {
-  console.log(Data[0]);
-
   switch (TableIndex) {
     case 1:
-      return AllArticleTable();
+      return AllArticleTable("News");
     case 2:
-      return AllArticleTable();
+      return AllArticleTable("books testing");
     case 3:
-      return AllArticleTable();
-    case 6:
-      return AllArticleTable();
+      return AllArticleTable("test");
     case 4:
+      return AllArticleTable("News");
+    case 5:
       return Podcast();
     default:
       return <div>No table found</div>;
   }
 }
-function AllArticleTable() {
-  return (
-    <div>
-      <table>
-        <thead>
+function AllArticleTable(DBName) {
+  var [MyLoader, setMyLoader] = useState(false);
+  var [Data, setData] = useState([]);
+  useEffect(() => {
+    async function getData() {
+      setData(Data = []);
+      const data = await getDataBase({ Data, setMyLoader, DBName });
+      setData(data.Data);
+      setMyLoader(data.setMyLoader);
+      setMyLoader(true);
+  
+    }
+    getData();
+  }, []);
+  return MyLoader ? (
+    <div class={styles.my}>
+      <table className={styles.Table}>
+        <thead className={styles.tableHead}>
           <tr>
             <th>Category</th>
-            <th>Description</th>
             <th>Name</th>
             <th>Title</th>
+            <th>Description</th>
             <th>Url</th>
             <th>UrlToImage</th>
           </tr>
         </thead>
-        <tbody>
+        <tbody className={styles.tableBody}>
           {Data.map((data, index) => (
             <tr key={index}>
-              <td>{data.id}</td>
-              <td>{data.name}</td>
-              <td>{data.class}</td>
-              <td>{data.title}</td>
+              <td className={styles.tableData}>{data.category}</td>
+              <td className={styles.tableData}>{data.name}</td>
+              <td className={styles.tableData}>{data.title}</td>
+              <td className={styles.tableData}>{data.description}</td>
+              <td className={styles.tableData}>{data.url}</td>
+              <td className={styles.tableData}>{data.urlToImage}</td>
             </tr>
           ))}
         </tbody>
       </table>
     </div>
+  ) : (
+    <div>No table found</div>
   );
 }
 
